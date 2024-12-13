4d457869d8767c9727fcb2048f02cbd8cf58e290
Bastian Wiesner <bastian.wiesner@siemens.com>
2024-12-13 05:20:33 +0100

    dep

--- GraduationGames/vite.config.ts
+++ GraduationGames/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ClaudiaGraduationGames/GraduationGames/', // Update with your correct path
+  base: '/ClaudiaGraduationGames/', // Update with your correct path
 });
