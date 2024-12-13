e86fe4a657a3128f7e41fbd3b74067f98797a7cd
Bastian Wiesner <bastian.wiesner@siemens.com>
2024-12-13 05:35:32 +0100

    test_dep

--- GraduationGames/vite.config.ts
+++ GraduationGames/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  //base: '/GraduationGames/', // Update with your correct path
+  base: '/GraduationGames/', // Adjust for exact GitHub Pages path context if needed
 });
